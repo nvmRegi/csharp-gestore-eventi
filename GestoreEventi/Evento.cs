beedc3ca0d2019cc9714044bbd7cc4aeb91defe8
regi <cabael.regine@gmail.com>
2022-04-22 10:11:04 +0200

    getter e setter evento

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        private string titoloEvento; //lettura e scrittura
+        private DateTime dataEvento; //lettura e scrittura
+        private int MaxCapienza; //lettura
+        private int nPostiPrenotati; //lettura
+
+        //------getter e setter-----
+        private void SetTitoloEvento(string titolo)
+        {
+            if(titolo == null)
+            {
+                throw new ArgumentNullException(nameof(titolo));
+            } else
+            {
+                this.titoloEvento = titolo;
+            }
+        }
+
+        public string GetTitoloEvento()
+        {
+            return this.titoloEvento;
+        }
+
+        private void SetDataEvento(DateTime data)
+        {
+            if(data < DateTime.Now)
+            {
+                throw new ArgumentOutOfRangeException(nameof(data));
+            } else
+            {
+                this.dataEvento = data;
+            }
+        }
+
+        public DateTime GetDataEvento()
+        {
+            return this.dataEvento;
+        }
+
+        private void SetMaxCapienza(int maxCapienza)
+        {
+            if(maxCapienza < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxCapienza));
+            } else
+            {
+                this.MaxCapienza = maxCapienza;
+            }
+        }
+
+        public int GetMaxCapienza()
+        {
+            return this.MaxCapienza;
+        }
+
+        public int GetNPostiPrenotati()
+        {
+            return this.nPostiPrenotati;
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,1 +1,1 @@
-﻿
+﻿using GestoreEventi;
